c2e640ddf8a6b03775804aef69ca8a1ed03734f2
Artiom Chilaru <artiom@flexlabs.org>
2025-12-09 23:02:58 +0000

    Day 4 with Raylib

--- 2025/04.cs
+++ 2025/04.cs
@@ -1,11 +1,29 @@
 #!/usr/bin/env dotnet
+#:package Raylib-cs@7.0.2
 #:project ../Helpers/AoC.Helpers.csproj
 using AoC.Helpers;
+using Raylib_cs;
 using System.Diagnostics;
 
 var sw = Stopwatch.StartNew();
 var map = Map.FromString(FileHelpers.ReadInputText("04.txt"));
 var parseTime = sw.Elapsed;
+
+Raylib.SetTargetFPS(60);
+Raylib.SetConfigFlags(ConfigFlags.HiddenWindow);
+Raylib.InitWindow(800, 600, "Raylib Test 2");
+
+var scale = (int)Math.Floor(Math.Min(
+    Raylib.GetMonitorWidth(Raylib.GetCurrentMonitor()) * 0.8f / map.Width,
+    Raylib.GetMonitorHeight(Raylib.GetCurrentMonitor()) * 0.8f / map.Height));
+var spacing = Math.Max(1, scale / 10);
+scale -= spacing;
+Raylib.CloseWindow();
+Raylib.ClearWindowState(ConfigFlags.HiddenWindow);
+
+Raylib.InitWindow(map.Width * (scale + spacing) + 20, map.Height * (scale + spacing) + 20, "AoC 2025 - Day 4");
+
+RenderMap(map);
 
 sw.Restart();
 var accessible = RemoveRolls(map);
@@ -24,11 +42,22 @@ var part2Time = sw.Elapsed;
 
 OutputHelpers.PrintTimings(parseTime, part1Time, part2Time);
 
-static int RemoveRolls(Map map)
+var cycles = 10;
+while (!Raylib.WindowShouldClose())
+{
+    if (cycles <= 0)
+        Raylib.EnableEventWaiting();
+    else
+        cycles--;
+    RenderMap(map);
+}
+Console.WriteLine("Exiting...");
+
+int RemoveRolls(Map map)
 {
     foreach (var point in map.AllPoints())
     {
-        if (map[point] == '.')
+        if (map[point] != '@')
         {
             continue;
         }
@@ -53,11 +82,50 @@ static int RemoveRolls(Map map)
     var accessible = 0;
     foreach (var point in map.AllPoints())
     {
-        if (map[point] is 'x')
+        if (map[point] == 'x')
         {
             accessible++;
-            map[point] = '.';
+            map[point] = '9';
         }
     }
+
+    RenderMap(map);
+
     return accessible;
+}
+
+void RenderMap(Map map)
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    foreach (var point in map.AllPoints())
+    {
+        var color = map[point] switch
+        {
+            '@' => Color.Gold,
+            >= '1' and <= '9' => new Color(
+                Color.Green.R - (Color.Green.R - Color.DarkGray.R) * (map[point] - '1') / 8,
+                Color.Green.G - (Color.Green.G - Color.DarkGray.G) * (map[point] - '1') / 8,
+                Color.Green.B - (Color.Green.B - Color.DarkGray.B) * (map[point] - '1') / 8),
+            '.' => Color.DarkGray,
+            _ => Color.DarkGray
+        };
+        if (map[point] is >= '1' and <= '9')
+        {
+            map[point]--;
+        }
+        if (map[point] == '0')
+        {
+            map[point] = '.';
+        }
+
+        Raylib.DrawRectangle(
+            point.X * (scale + spacing) + 10,
+            point.Y * (scale + spacing) + 10,
+            scale,
+            scale,
+            color);
+    }
+    Raylib.EndDrawing();
+    Raylib.WaitTime(0.03f);
 }
